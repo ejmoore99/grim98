30ff051f9afe4fae97441f69c86dd529a479f77c
ejmoore99 <ervinworks99@gmail.com>
2024-07-01 20:22:45 -0400

    added handlebars.js

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require('express');
-const path = require('path');
 const app = express();
+const exphbs = require('express-handlebars');
+const path = require('path');
 
 const PORT = process.env.PORT || 5000; // Heroku ( or whatever cloud service) will set the PORT environment variable
+
+// Handlebars Middleware
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+// Set handlebars routes
+app.get('/', function (req, res) {
+  res.render('home');
+});
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
